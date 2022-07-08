65d21139c90e3f010860b35d53c39d70611c962a
Trong Pham <trong.phamtranduc@gmail.com>
2022-07-08 21:16:32 +0700

    fix css problem

--- webapp/src/app/pages/home/components/problem/components/submission/submission.component.ts
+++ webapp/src/app/pages/home/components/problem/components/submission/submission.component.ts
@@ -1,47 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NbGetters, NbSortDirection, NbTreeGridDataSource, NbTreeGridDataSourceBuilder } from '@nebular/theme';
+interface FSEntry {
+  name: string;
+  size: string;
+  kind: string;
+  items?: number;
+  childEntries?: FSEntry[];
+  expanded?: boolean;
+}
 @Component({
   selector: 'app-submission',
   templateUrl: './submission.component.html',
   styleUrls: ['./submission.component.scss']
 })
 export class SubmissionComponent implements OnInit {
+  customColumn = 'Submit By';
+  defaultColumns = ['Time (ms) ', 'Memory (KB) ', 'Language', 'Score'];
+  allColumns = [this.customColumn, ...this.defaultColumns];
+  source: NbTreeGridDataSource<FSEntry>;
 
-  constructor() { }
-
+  constructor(dataSourceBuilder: NbTreeGridDataSourceBuilder<FSEntry>) {
+    const getters: NbGetters<FSEntry, FSEntry> = {
+      dataGetter: (node: FSEntry) => node,
+      childrenGetter: (node: FSEntry) => node.childEntries || undefined,
+      expandedGetter: (node: FSEntry) => !!node.expanded,
+    };
+    this.source = dataSourceBuilder.create(this.data, getters);
+  }
   ngOnInit(): void {
+    // throw new Error('Method not implemented.');
   }
 
-  submissions = [
+  sortDirection: NbSortDirection = NbSortDirection.NONE;
+  sortColumn!: string;
+  updateSort(sortRequest: any): void {
+    // this.sortColumn = sortRequest.column;
+    // this.sortDirection = sortRequest.direction;
+  }
+
+  private data: any = [
     {
-      submitBy: "Đức Trọng",
-      timeLimit: 8.99,
-      memoryLimit: 256,
-      language: "Javascript",
-      score: 80
+      "Submit By": "Duc Trong",
+      "Time (ms)": 3.6,
+      "Memory": 100,
+      "Language": "Javascript",
+      "Score": 80
     },
     {
-      submitBy: "Trung Tôm",
-      timeLimit: 8.99,
-      memoryLimit: 150,
-      language: "C++",
-      score: 100
-
+      "Submit By": "Duc Trong",
+      "Time": 1.8,
+      "Memory": 50,
+      "Language": "Ruby",
+      "Score": 80
     },
     {
-      submitBy: "Trung Xôi",
-      timeLimit: 8.99,
-      memoryLimit: 100,
-      language: "Ruby",
-      score: 100
+      "Submit By": "Duc Trong",
+      "Time": 2.1,
+      "Memory": 200,
+      "Language": "C++",
+      "Score": 80
     }
-  ]
+  ];
 
-  filter(value: string) {
-    if (value == "timeLimit") {
-    } else if (value == "memoryLimit") {
-    } else {
+  getShowOn(index: any) { }
+  getSortDirection(column: string): NbSortDirection {
+    if (this.sortColumn === column) {
+      return this.sortDirection;
     }
+    return NbSortDirection.NONE;
   }
 
 }
