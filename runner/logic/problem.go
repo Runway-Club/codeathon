b7c77624d6ada82e93a12a55702cd297e3b6a350
AmbroseNTK <kiettuannguyense@gmail.com>
2022-07-10 13:42:00 +0700

    print waiting submission

--- runner/logic/problem.go
+++ runner/logic/problem.go
@@ -112,7 +112,7 @@ func (l *ProblemLogic) Evaluate(submissionId string) error {
 		return err
 	}
 
-	println(waitingSubmission)
+	println(*waitingSubmission)
 
 	// get problem from firestore
 	problem, err := l.Get(waitingSubmission.ProblemId)
