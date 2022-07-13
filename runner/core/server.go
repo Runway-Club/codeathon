c1cb5d60aec03397832b8a240563ce24cbd88465
AmbroseNTK <kiettuannguyense@gmail.com>
2022-07-13 12:30:06 +0700

    Change CORS

--- runner/core/server.go
+++ runner/core/server.go
@@ -33,6 +33,7 @@ func NewServer(configFile string, app *firebase.App) (*Server, error) {
 		return func(c echo.Context) error {
 			c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 			c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 			if c.Request().Method == "OPTIONS" {
 				return nil
 			}
