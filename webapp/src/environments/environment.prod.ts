ac249a4be1a36f7f6b84fb476425e00909012b1e
Trong Pham <trong.phamtranduc@gmail.com>
2022-09-02 20:53:27 +0700

    cm

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -11,6 +11,7 @@ export const environment = {
     apiKey: "515e04efc9msh90cb0e1f01e697bp1a73e6jsn3346c5abc487",
     host: "judge0-ce.p.rapidapi.com"
   },
-  api: "https://server.codeathon.runwayclub.dev/api/v1",
+  // api: "https://api.praktikum.gunadarma.ac.id",
+  api: "http://35.223.167.125:8080/api/v1",
   production: true
 };
